201d07fb582fd050d96aa09e34ccd02d17516043
Dionny Prensa <d.prensa1@gmail.com>
2020-09-27 22:42:20 -0400

    19. Feature: Creating a Task (Part 2: Controller)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import {Controller, Get} from "@nestjs/common";
+import {Body, Controller, Get, Post, Patch, Delete} from "@nestjs/common";
+import {ITask} from "./task.model";
 import {TasksService} from "./tasks.service";
 
 @Controller("tasks")
@@ -6,7 +7,17 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): ITask[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body("title") title: string,
+    @Body("description") description: string
+  ): ITask {
+    const newTask: ITask = this.tasksService.createTask(title, description);
+
+    return newTask;
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,26 @@
 import {Injectable} from "@nestjs/common";
+import {ITask, TaskStatusEnum} from "./task.model";
+// import * as uuid from "uuid/v1";
+import {v1 as uuid} from "uuid";
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: ITask[] = [];
 
-  getAllTasks() {
+  getAllTasks(): ITask[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): ITask {
+    const task: ITask = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatusEnum.OPEN
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
