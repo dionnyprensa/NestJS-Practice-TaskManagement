b17cb568ea82dec9e6cbcbb7978a22e369572879
Dionny Prensa <d.prensa1@gmail.com>
2022-07-19 00:04:23 -0400

    [ADD] filter Task by Status, Title or Description

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDTO } from './create-task.dto';
+import { GetTasksFilterDTO } from './get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,16 +10,23 @@ export class TasksController {
   }
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksServices.getAll()
+  getTasks(@Query() tasksFilterDTO: GetTasksFilterDTO): Task[] {
+    if (Object.keys(tasksFilterDTO).length) {
+
+      return this.tasksServices.getWithFilters(tasksFilterDTO)
+    } else {
+
+      return this.tasksServices.getAll()
+    }
+
   }
 
-  @Get("/:id")
+  @Get('/:id')
   getTaskById(@Param('id') id: string): Task {
     return this.tasksServices.getByID(id)
   }
 
-  @Delete("/:id")
+  @Delete('/:id')
   deleteTaskById(@Param('id') id: string): void {
     return this.tasksServices.deleteByID(id)
   }
@@ -30,10 +38,10 @@ export class TasksController {
     return this.tasksServices.create(createTaskDTO)
   }
 
-  @Patch("/:id/status")
+  @Patch('/:id/status')
   updateTask(
-    @Param("id") id: string,
-    @Body("status") status: TaskStatus
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus
   ): Task {
     return this.tasksServices.updateByID(id, status)
   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from "uuid"
 import { CreateTaskDTO } from './create-task.dto';
+import { GetTasksFilterDTO } from './get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,21 @@ export class TasksService {
 
   getAll(): Task[] {
     return this.tasks;
+  }
+
+  getWithFilters(tasksFilterDTO: GetTasksFilterDTO): Task[] {
+    const { status, search } = tasksFilterDTO
+    let _task = [...this.tasks];
+
+    if (status) {
+      _task = this.tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      _task = this.tasks.filter((task) => task.title.toLowerCase().includes(search) || task.description.toLowerCase().includes(search))
+    }
+
+    return _task
   }
 
   getByID(id: string): Task {
