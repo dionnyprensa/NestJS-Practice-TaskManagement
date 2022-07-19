df53abe32ab6c0f896237cb5656fe010b1a2ef2f
Dionny Prensa <d.prensa1@gmail.com>
2022-07-18 23:44:25 -0400

    [ADD] base crud operations using memory as db

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './task.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateTaskDTO } from './create-task.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -12,8 +13,28 @@ export class TasksController {
     return this.tasksServices.getAll()
   }
 
+  @Get("/:id")
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksServices.getByID(id)
+  }
+
+  @Delete("/:id")
+  deleteTaskById(@Param('id') id: string): void {
+    return this.tasksServices.deleteByID(id)
+  }
+
   @Post()
-  createTask(@Body('title') title, @Body('description') description): Task {
-    return this.tasksServices.createTask(title, description)
+  createTask(
+    @Body() createTaskDTO: CreateTaskDTO
+  ): Task {
+    return this.tasksServices.create(createTaskDTO)
+  }
+
+  @Patch("/:id/status")
+  updateTask(
+    @Param("id") id: string,
+    @Body("status") status: TaskStatus
+  ): Task {
+    return this.tasksServices.updateByID(id, status)
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from "uuid"
+import { CreateTaskDTO } from './create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,13 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getByID(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  create(createTaskDTO: CreateTaskDTO): Task {
+    const { title, description } = createTaskDTO;
+
     const task: Task = {
       id: uuid(),
       title,
@@ -21,5 +28,17 @@ export class TasksService {
     this.tasks.push(task);
 
     return task
+  }
+
+  deleteByID(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateByID(id: string, status: TaskStatus): Task {
+    const taskID = this.tasks.findIndex((task) => task.id === id);
+
+    this.tasks[taskID].status = status;
+
+    return this.tasks[taskID];
   }
 }
